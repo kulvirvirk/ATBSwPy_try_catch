b31824a083f25312f43f57c50e0bd805c194d17e
kulvirvirk <kvirkdev@gmail.com>
2020-04-02 05:07:01 +0000

    tested the function by calling with valid inputs

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # 1. write a divide_by function that divides 50 by number passed to iter
 
-def div_by(number):
+def divide_by(number):
   return 50/number
 
+print(divide_by(2))
+print(divide_by(10))
+print(divide_by(5.5))
